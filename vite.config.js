ff23940964c5c5bbbbd754710257da4a79a3b19e
ThiruveedulaChandraSekhar <163702568+ThiruveedulaChandraSekhar@users.noreply.github.com>
2025-09-09 20:09:41 +0530

    Add server proxy configuration to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://13.233.195.36:8081',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
